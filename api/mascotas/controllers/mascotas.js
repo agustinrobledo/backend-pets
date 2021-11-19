8fe923cb1306045469959341c88b57fa0f25375b
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-11-19 14:18:26 -0300

    added styles and queries

--- api/mascotas/controllers/mascotas.js
+++ api/mascotas/controllers/mascotas.js
@@ -8,35 +8,44 @@
 module.exports = {
     async create(ctx){
         let {Nombre, Raza, Edad, Sexo, Mes, Año, Aproximado} = ctx.request.body;
-        const raza = await strapi.services.razas.findOne({nombre_raza: Raza});
-        if(Edad === ""){
-            const actualMonth = new Date().getMonth();
-            const actualDay = new Date().getDate();
-            const actualYear = new Date().getFullYear();
-                if(Mes !== 0){
-                const monthOfPet = actualMonth - Mes;
-                if(monthOfPet === 0){
-                    monthOfPet = 1;
-                }
-                else if(monthOfPet < 0){
-                    monthOfPet = 12 + monthOfPet;
-                    actualYear = actualYear - 1;
-                }
-                Edad = new Date(actualYear, monthOfPet, actualDay + 1)
+        if(!Nombre || !Raza || !Sexo){
+            ctx.response.status = 400;
+            ctx.response.body = {
+                message: 'Faltan datos'
             }
-                else if(Año){
-                    Edad = new Date(actualYear - Año, actualMonth, actualDay + 1);
+        }else{
+            const raza = await strapi.services.razas.findOne({nombre_raza: Raza});
+            if(Edad === ""){
+                const actualMonth = new Date().getMonth();
+                const actualDay = new Date().getDate() + 1;
+                const actualYear = new Date().getFullYear();
+                    if(Mes !== 0){
+                    const monthOfPet = actualMonth - Mes;
+                    if(monthOfPet === 0){
+                        monthOfPet = 1;
+                    }
+                    else if(monthOfPet < 0){
+                        monthOfPet = 12 + monthOfPet;
+                        actualYear = actualYear - 1;
+                    }
+                    Edad = new Date(actualYear, monthOfPet, actualDay + 1)
                 }
-        }
-        const mascota = await strapi.services.mascotas.create({
-            Nombre,
-            Raza: raza.id,
-            Edad,
-            Sexo,
-            Aproximado
-        });
+                    else if(Año){
+                        Edad = new Date(actualYear - Año, actualMonth, actualDay + 1);
+                    }
+            }
+            
+            const mascota = await strapi.services.mascotas.create({
+                Nombre,
+                Raza: raza.id,
+                Edad,
+                Sexo,
+                Aproximado
+            });
         return mascota;
+        }
     }
+
 };
 
 
